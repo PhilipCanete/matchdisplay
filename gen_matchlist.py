eec230651f9c1d365c72a35a0ef5dcb76e769389
Alex Chew <alex@alexchew.net>
2015-03-06 08:25:55 -0800

    Add generation from csv; refactor generation script

--- gen_matchlist.py
+++ gen_matchlist.py
@@ -1,61 +1,111 @@
-import csv
 import os
 import sys
 
 from jinja2 import Environment, FileSystemLoader
-import requests
 
-# API endpoint and required identification header
-URL = 'http://www.thebluealliance.com/api/v2/event/{}/matches'
-HEADERS = {'X-TBA-App-Id': 'frc295:matchdisplay:v01'}
+USAGE_STRING = '''Usage: python gen_matchlist.py [csv <filename> | tba <eventcode>] <team>
+Example: python gen_matchlist.py csv compschedule.csv 295
+Example: python gen_matchlist.py tba 2013cama 295'''
 
-# Usage
-if len(sys.argv) != 3:
-	print('Usage: python get_tba_schedule.py <eventcode> <team>')
-	print('Example: python gen_matchlist.py 2014cama 295')
-	sys.exit()
+def get_from_csv(filename):
+	'''
+	Get data from a csv file.
+	Rows should have the following format:
 
-# GET request to endpoint, using event code from argv
-req = requests.get(URL.format(sys.argv[1]), headers=HEADERS)
-matches = req.json()
+	matchnum,red1,red2,red3,blue1,blue2,blue3
+	'''
+	import csv
+	data = []
 
-# Check for 404 error (e.g. no such event)
-if isinstance(matches, dict) and matches['404']:
-	sys.exit('404: {}'.format(matches['404']))
+	# Read csv file into list
+	with open(filename, 'rb') as f:
+		rows = list(csv.reader(f))
+	
+	# Add data to match data list (casted to integers)
+	for r in rows:
+		num = int(r[0])
+		red  = [int(team) for team in r[1:4]]
+		blue = [int(team) for team in r[4:7]]
+		data.append({'num': num, 'red': red, 'blue': blue})
 
-data = []
-# Response is JSON array of matches
-for m in matches:
-	# Only get qualification matches
-	if str(m['comp_level']) != 'qm': continue
+	return data
 
-	# Initialize match data dict with match number
-	md = {'num': m['match_number']}
 
-	for a in ['red', 'blue']:
-		# For each alliance, add to match data: key is alliance,
-		# value is array of team numbers
-		md[a] = [int(str(t[3:])) for t in m['alliances'][a]['teams']]
-	# Add match data to list of data
-	data.append(md)
+def get_from_tba(event):
+	'''
+	Get data from The Blue Alliance's Event API.
+	'''
+	import requests
 
-# Only keep matches in which our team participates
-data = filter(lambda d: int(sys.argv[2]) in d['red']
-		or int(sys.argv[2]) in d['blue'], data)
+	# API endpoint and required identification header
+	URL = 'http://www.thebluealliance.com/api/v2/event/{}/matches'.format(event)
+	HEADERS = {'X-TBA-App-Id': 'frc295:matchdisplay:v01'}
 
-# Sort matches by match number
-data = sorted(data, key=lambda d: d['num'])
+	# GET request to endpoint
+	req = requests.get(URL, headers=HEADERS)
+	matches = req.json()
 
-# No matches (no schedule available)
-if len(data) == 0:
-	sys.exit('No matches received')
+	# Check for 404 error (e.g. no such event)
+	if isinstance(matches, dict) and matches['404']:
+		sys.exit('404: {}'.format(matches['404']))
 
-# Initialize Jinja engine
-env = Environment(loader=FileSystemLoader('assets'), trim_blocks=True,
-		lstrip_blocks=True)
-template = env.get_template('base.html')
+	data = []
+	# Response is JSON array of matches
+	for m in matches:
+		# Only get qualification matches
+		if str(m['comp_level']) != 'qm': continue
 
-# Render template to file
-with open(os.path.join('output', '{}_{}.html').format(sys.argv[1], sys.argv[2]), 'wb') as f:
-	f.write(template.render(matches=data, event=sys.argv[1], ourteam=int(sys.argv[2])))
+		# Initialize match data dict with match number
+		md = {'num': m['match_number']}
+
+		for a in ['red', 'blue']:
+			# For each alliance, add to match data: key is alliance,
+			# value is array of team numbers
+			md[a] = [int(str(t[3:])) for t in m['alliances'][a]['teams']]
+		# Add match data to list of data
+		data.append(md)
+
+	# No matches (no schedule available)
+	if len(data) == 0:
+		sys.exit('No matches received')
+
+	return data
+
+
+def main():
+	# Ensure correct number of arguments
+	if len(sys.argv) != 4:
+		sys.exit(USAGE_STRING)
+
+	ourteam = int(sys.argv[3])
+	event = sys.argv[2]
+
+	# Check input type argument
+	if sys.argv[1] == 'tba':
+		data = get_from_tba(event)
+	elif sys.argv[1] == 'csv':
+		data = get_from_csv(event)
+	else:
+		sys.exit(USAGE_STRING)
+
+	# Only keep matches in which our team participates
+	data = filter(lambda d: ourteam in d['red'] or ourteam in d['blue'], data)
+
+	# Sort matches by match number
+	data = sorted(data, key=lambda d: d['num'])
+
+	# Initialize Jinja engine
+	env = Environment(loader=FileSystemLoader('assets'), trim_blocks=True,
+			lstrip_blocks=True)
+	template = env.get_template('base.html')
+
+	# Get bare filename (no path, no file extension)
+	event = ''.join(event.split(os.path.sep)[-1].split('.')[:-1])
+
+	# Render template to file
+	with open(os.path.join('output', '{}_{}.html').format(event, ourteam), 'wb') as f:
+		f.write(template.render(matches=data, event=event, ourteam=ourteam))
+
+
+if __name__ == '__main__': main()
 
